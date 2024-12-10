0eb63c09906496d5b4e420fe2b1c88887858cd74
Tomas KOVAC <office@ztk-comp.sk>
2024-12-10 22:10:35 +0100

    Create gps_provider.sql

---
+++ mysql/gps_provider.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `gps_provider` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `provider` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
