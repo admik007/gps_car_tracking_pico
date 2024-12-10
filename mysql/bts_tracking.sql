9899a6cb23304a360c0885dd44ca2ed6e0fcb34a
Tomas KOVAC <office@ztk-comp.sk>
2024-12-10 22:09:54 +0100

    Create bts_tracking.sql

---
+++ mysql/bts_tracking.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `bts_tracking` (
+  `id` int(8) NOT NULL,
+  `lat` varchar(14) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `lon` varchar(14) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `alt` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `time` varchar(26) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `device` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `mcc` char(3) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `mnc` char(2) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `bsic` char(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `cellid` char(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `lac` char(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `address` char(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
