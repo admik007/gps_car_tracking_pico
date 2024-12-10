dc20ccc5431c7508220cbbd9e2db324d628513b9
Tomas KOVAC <office@ztk-comp.sk>
2024-12-10 22:12:32 +0100

    Update bts_tracking.sql

--- mysql/bts_tracking.sql
+++ mysql/bts_tracking.sql
@@ -12,3 +12,12 @@ CREATE TABLE `bts_tracking` (
   `lac` char(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `address` char(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Indexes for table `bts_tracking`
+ALTER TABLE `bts_tracking`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+-- AUTO_INCREMENT for table `bts_tracking`
+ALTER TABLE `bts_tracking`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
